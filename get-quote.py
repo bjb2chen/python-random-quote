c4c5252d497cb8af029df951a2e2e556d97cadce
bjb2chen <bjb2chen@uwaterloo.ca>
2022-06-22 20:38:10 -0400

    Finalized code generator for the course

--- get-quote.py
+++ get-quote.py
@@ -4,12 +4,14 @@ def primary():
   # print("Keep it logically awesome.")
 
   f = open("quotes.txt")
-  quotes = f.readlines()
+  quotes = f.readlines(\n)
   f.close()
 
   last = 13
   rnd = random.randint(0, last)
+  rnd2 = random.randint(0, last)
   print(quotes[rnd])
+  print(quotes[rnd2])
 
 if __name__== "__main__":
   primary()
